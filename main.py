4a8e42d21a34b640762be7c3793527a398d74831
Yash Gaitonde <gaitonde@umich.edu>
2020-06-06 17:24:25 +0000

    read file

--- main.py
+++ main.py
@@ -2,44 +2,56 @@ import statistics
 counter = 0
 
 def choosePivot(a,l,r,s):
-  n = r-l
-  first = l
-  last = r
-  mid = n//2-1 if n%2==0 else n//2
-  if(s=="l"):
+   n = r-l
+   first = l
+   last = r
+   mid = n//2-1 if n%2==0 else n//2
+   if(s=="l"):
     return first
-  if(s=="r"):
+   if(s=="r"):
     return last
-  if(s=="m"):
-    return statistics.median([a[first],a[last],a[mid]])
-
+   if(s=="m"):
+     if(statistics.median([a[first],a[last],a[mid]]) == a[first]):
+       return first
+     if(statistics.median([a[first],a[last],a[mid]]) == a[last]):
+       return last
+     if(statistics.median([a[first],a[last],a[mid]]) == a[mid]):
+       return mid
+  
 def swap(a,i,j):
   temp = a[i]
   a[i] = a[j]
   a[j] = temp
-
-def partition(a,l,r):
-  p = a[l]
+  
+def partition(a,l,r,p_index):
+  p = a[p_index]
+  swap(a,l,p_index)
   i = l+1
   for j in range(l+1,r+1):
+    global counter
+    counter+=1
     if(a[j] < p):
       swap(a, i, j)
       i+=1
-    global counter
-    counter+=1
   swap(a,l,i-1)
-  return i-1
+  return a.index(p)
 
 def quicksort(a,l,r):
-  if r-l<=1:
-    return
-  p = choosePivot(a,l,r,"l")
-  swap(a,l,p)
-  val = partition(a,l,r)
-  quicksort(a,0,val-1)
-  quicksort(a,val+1, r-l+1)
+  n = r-l
+  if (n<1): return
+  p = choosePivot(a,l,r,"r")
+  j = partition(a,l,r,p)
+  quicksort(a,l,j-1)
+  quicksort(a,j+1,r)
 
-a = [6,5,12,4,3,2,1]
+with open('QuickSort.txt') as f:
+    a = [x.strip() for x in f]
+    a = [ int(x) for x in a ]
 quicksort(a,0,len(a)-1)
-print(a)
 print(counter)
+
+
+#151001 comparison using m
+#164123 comparison using r
+#162085 comparison using l
+
